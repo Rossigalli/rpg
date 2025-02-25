27df34d554092a2a6721cbb98dc230735e062c5e
Matheus Rossigalli <rossigalli.dev@gmail.com>
2025-02-25 11:40:43 -0300

    Adicionado playerSheet

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
+function toggleSidebar(){
+    let sidebar = document.getElementById("Sidebar")
+    sidebar.classList.toggle('active')
+    console.log('a')
+}
+
 class PlayerManager {
     constructor() {
         this.players = JSON.parse(localStorage.getItem("players")) || [];
         this.sidebar = document.getElementById("Sidebar");
+        this.main = document.getElementById("Main");
         this.renderPlayers();
-
-        // Adicionando event listeners para os botões
-        document.getElementById("Export-btn").addEventListener("click", () => this.exportPlayers());
-        document.getElementById("Import-btn").addEventListener("click", () => this.importPlayers());
     }
 
     savePlayers() {
@@ -15,9 +18,27 @@ class PlayerManager {
 
     addPlayer() {
         const name = `Jogador ${this.players.length + 1}`;
-        const vida = 100;
-        const mana = 100;
-        const player = { name, vida, mana };
+        const player = {
+            vida: 100,
+            mana: 100,
+            name,
+            clan: "Clan Desconhecido",
+            caminho: "Caminho Desconhecido",
+            vitalidade: 100,
+            energia: 100,
+            ataque: 10,
+            defesa: 5,
+            arma: "Espada",
+            armadura: "Armadura de Ferro",
+            forca: 10,
+            destreza: 10,
+            resistencia: 10,
+            resiliencia: 10,
+            precisao: 10,
+            sabedoria: 10,
+            percepcao: 10,
+            passivas: ["Regeneração Rápida"]
+        };
         this.players.push(player);
         this.savePlayers();
         this.renderPlayers();
@@ -29,12 +50,14 @@ class PlayerManager {
     }
 
     deletePlayer(index) {
-        const confirmation = confirm("Tem certeza que deseja deletar este jogador?");
-    
+        const confirmation = confirm(`Tem certeza que deseja deletar o jogador ${this.players[index].name}?`);
         if (confirmation) {
-            this.players.splice(index, 1); 
-            this.savePlayers(); 
-            this.renderPlayers(); 
+            this.players.splice(index, 1);
+            this.currentPlayerActive = null
+            
+            this.savePlayers();
+            this.renderPlayers();
+            this.hiddePlayerDetails()
         }
     }
 
@@ -48,13 +71,45 @@ class PlayerManager {
         }
     }
 
+    updateSheetStat(index, stat, value) {
+        this.players[index][stat] = value;
+        this.savePlayers();
+        
+        if(stat == "name"){
+            this.renderPlayers()
+        }
+    }
+
+    updatePlayerPassiva(index, passivaIndex, value) {
+        this.players[index].passivas[passivaIndex] = value;
+        this.savePlayers();
+    }
+
+    addPassiva(index) {
+        this.players[index].passivas.push("Nova Passiva");
+        this.savePlayers();
+        this.showPlayerDetails(index);
+    }
+
+    removePassiva(index, passivaIndex) {
+        this.players[index].passivas.splice(passivaIndex, 1);
+        this.savePlayers();
+        this.showPlayerDetails(index);
+    }
+
+
     renderPlayers() {
-        this.sidebar.innerHTML = '<div id="NewPlayer" class="hover" onclick="manager.addPlayer()"> <p>+ Novo jogador</p></div>';
+        this.sidebar.innerHTML = `
+        <div id="NewPlayer" class="hover border" onclick="manager.addPlayer()"> <p>+ Novo jogador</p></div>`;
         this.players.forEach((player, index) => {
             const playerElement = document.createElement("div");
-            playerElement.classList.add("player", "hover");
+            playerElement.classList.add("player", "hover", "border");
+            if(this.currentPlayerActive == index){
+                playerElement.classList.add("active");
+                this.playerActive = playerElement
+            }
             playerElement.innerHTML = `
-                <div class="player-image">
+                <div class="player-image" onclick="manager.showPlayerDetails(${index}, event)">
                     <img width="80px" height="80px" src="./assets/character_profile.webp" alt="Imagem de perfil do personagem">
                 </div>
                 <div class="player-description">
@@ -80,10 +135,11 @@ class PlayerManager {
 
     enableEditing(index) {
         const input = document.getElementById(`player-name-${index}`);
-        const end = input.value.length
-        input.setSelectionRange(end, end)
+        const end = input.value.length;
+        input.setSelectionRange(end, end);
         input.disabled = false;
         input.focus();
+
         input.addEventListener("blur", () => {
             this.updatePlayerName(index, input.value);
             input.disabled = true;
@@ -105,45 +161,71 @@ class PlayerManager {
         }
     }
 
-    // Função para exportar os jogadores para um arquivo JSON
-    exportPlayers() {
-        const data = JSON.stringify(this.players, null, 2);
-        const blob = new Blob([data], { type: 'application/json' });
-        const link = document.createElement("a");
-        link.href = URL.createObjectURL(blob);
-        link.download = "players.json";
-        link.click();
+    showPlayerDetails(index, event) {
+        if(event){
+            this.playerActive?.classList.remove("active")
+
+            this.playerActive = event.currentTarget.parentElement
+            this.playerActive.classList.add("active")
+            this.currentPlayerActive = index;
+            
+            toggleSidebar()
+        }
+
+        const player = this.players[index];
+
+        this.main.innerHTML = `
+            <div class="player-sheet border"> 
+                <div class="info-block border col-10">
+                    <h3 class="info-block-title">Info</h3>
+                    <p><strong>Nome:</strong><input type="text" value="${player.name}" onkeyup="manager.updateSheetStat(${index}, 'name', this.value)"></p>
+                    <p><strong>Clan:</strong><input type="text" value="${player.clan}" onkeyup="manager.updateSheetStat(${index}, 'clan', this.value)"></p>
+                    <p><strong>Caminho:</strong><input type="text" value="${player.caminho}" onkeyup="manager.updateSheetStat(${index}, 'caminho', this.value)"></p>
+                </div>
+                
+                <div class="info-block border col-10">
+                    <div>
+                        <p><strong>Vitalidade:</strong><input type="number" value="${player.vitalidade}" onkeyup="manager.updateSheetStat(${index}, 'vitalidade', this.value)"></p>
+                        <p><strong>Energia:</strong><input type="number" value="${player.energia}" onkeyup="manager.updateSheetStat(${index}, 'energia', this.value)"></p>
+                        <p><strong>Ataque:</strong><input type="number" value="${player.ataque}" onkeyup="manager.updateSheetStat(${index}, 'ataque', this.value)"></p>
+                        <p><strong>Defesa:</strong><input type="number" value="${player.defesa}" onkeyup="manager.updateSheetStat(${index}, 'defesa', this.value)"></p>
+                    </div>
+
+                    <div>
+                        <p><strong>Arma:</strong><input type="text" value="${player.arma}" onkeyup="manager.updateSheetStat(${index}, 'arma', this.value)"></p>
+                        <p><strong>Armadura:</strong><input type="text" value="${player.armadura}" onkeyup="manager.updateSheetStat(${index}, 'armadura', this.value)"></p>
+                    </div>
+                </div>
+
+                <div class="info-block border col-3">
+                    <h3 class="info-block-title">Status</h3>
+                    <p><strong>Força:</strong><input type="number" value="${player.forca}" onkeyup="manager.updateSheetStat(${index}, 'forca', this.value)"></p>
+                    <p><strong>Destreza:</strong><input type="number" value="${player.destreza}" onkeyup="manager.updateSheetStat(${index}, 'destreza', this.value)"></p>
+                    <p><strong>Resistência:</strong><input type="number" value="${player.resistencia}" onkeyup="manager.updateSheetStat(${index}, 'resistencia', this.value)"></p>
+                    <p><strong>Resiliência:</strong><input type="number" value="${player.resiliencia}" onkeyup="manager.updateSheetStat(${index}, 'resiliencia', this.value)"></p>
+                    <p><strong>Precisão:</strong><input type="number" value="${player.precisao}" onkeyup="manager.updateSheetStat(${index}, 'precisao', this.value)"></p>
+                    <p><strong>Sabedoria:</strong><input type="number" value="${player.sabedoria}" onkeyup="manager.updateSheetStat(${index}, 'sabedoria', this.value)"></p>
+                    <p><strong>Percepção:</strong><input type="number" value="${player.percepcao}" onkeyup="manager.updateSheetStat(${index}, 'percepcao', this.value)"></p>
+                </div>
+                
+                <div class="info-block border col-7">
+                    <h3 class="info-block-title">Passivas</h3>
+                    <ul>
+                        ${player.passivas.map((passiva, i) => `
+                            <li>
+                                <input type="text" value="${passiva}" onkeyup="manager.updatePlayerPassiva(${index}, ${i}, this.value)">
+                                <img width="15px" height="15px" src="./assets/icons/trash-solid.svg" alt="Deletar" onclick="manager.removePassiva(${index}, ${i})">
+                            </li>`).join('')}
+                    </ul>
+                    <button class="border" onclick="manager.addPassiva(${index})">+ Adicionar Passiva</button>
+                </div>
+            </div>
+        `;
     }
 
-    // Função para importar os jogadores a partir de um arquivo JSON
-    importPlayers() {
-        const input = document.createElement("input");
-        input.type = "file";
-        input.accept = ".json";
-        input.addEventListener("change", (event) => {
-            const file = event.target.files[0];
-            if (file) {
-                const reader = new FileReader();
-                reader.onload = () => {
-                    try {
-                        const data = JSON.parse(reader.result);
-                        if (Array.isArray(data)) {
-                            this.players = data;
-                            this.savePlayers();
-                            this.renderPlayers();
-                        } else {
-                            alert("O arquivo JSON não contém dados válidos.");
-                        }
-                    } catch (error) {
-                        alert("Erro ao ler o arquivo JSON.");
-                    }
-                };
-                reader.readAsText(file);
-            }
-        });
-        input.click();
+    hiddePlayerDetails(){
+        this.main.innerHTML = '';
     }
 }
 
-// Inicializar o gerenciador e renderizar os jogadores salvos
 const manager = new PlayerManager();
